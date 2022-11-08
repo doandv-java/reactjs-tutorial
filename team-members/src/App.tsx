5d1a0eafe54b73c9ba6d2854e787a650faa14dd0
doandv-java <doandv188@gmail.com>
2022-11-08 10:10:01 +0700

    update code demo team members

--- team-members/src/App.tsx
+++ team-members/src/App.tsx
@@ -1,14 +1,122 @@
-import React from 'react';
+import React, {SetStateAction, useState} from "react";
 import './App.css';
 import Header from "./Header";
 import Employees from "./Employees";
 import Footer from "./Footer";
 
+const initialState = [
+    {
+        id: 1,
+        fullName: "Bob Jones",
+        designation: "Javascript Developer",
+        gender: "male",
+        teamName: "teamA"
+    },
+    {
+        id: 2,
+        fullName: "Jill Bailey",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "teamA"
+    },
+    {
+        id: 3,
+        fullName: "Gail Shepherd",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamA"
+    },
+    {
+        id: 4,
+        fullName: "Alex",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "teamB"
+    },
+    {
+        id: 5,
+        fullName: "Bob",
+        designation: "Javascript Developer",
+        gender: "male",
+        teamName: "teamB"
+    },
+    {
+        id: 6,
+        fullName: "Jill",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "teamB"
+    },
+    {
+        id: 7,
+        fullName: "Shepherd",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamC"
+    },
+    {
+        id: 8,
+        fullName: "Kai",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamC"
+    },
+    {
+        id: 9,
+        fullName: "Oriana",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamC"
+    },
+    {
+        id: 10,
+        fullName: "Dr.Jax",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamD"
+    },
+    {
+        id: 11,
+        fullName: "Dr.Swain",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamD"
+    },
+    {
+        id: 12,
+        fullName: "Lee shin",
+        designation: "Java Developer",
+        gender: "male",
+        teamName: "teamD"
+    }
+]
+
 function App() {
+    const [employees, setEmployees] = useState(initialState);
+    const [selectedTeam, setTeam] = useState('teamB');
+
+    function handleTeamSelectionChange(event: { target: { value: SetStateAction<string>; }; }) {
+        setTeam(event.target.value);
+    }
+
+    function handleEmployeeCardClick(event: { currentTarget: { id: string; }; }) {
+        const transformedEmployees = employees.map((emp) => emp.id === parseInt(event.currentTarget.id)
+            ? (emp.teamName === selectedTeam) ? {...emp, teamName: ""} : {...emp, teamName: selectedTeam} : emp);
+        setEmployees(transformedEmployees);
+    }
+
     return (
         <div className="App">
-            <Header/>
-            <Employees/>
+            <Header
+                selectedTeam={selectedTeam}
+                teamMemberCount={employees.filter((emp) => emp.teamName === selectedTeam).length}
+            />
+            <Employees
+                employees={employees}
+                selectedTeam={selectedTeam}
+                handleEmployeeCardClick={handleEmployeeCardClick}
+                handleTeamSelectionChange={handleTeamSelectionChange}
+            />
             <Footer/>
         </div>
     );
--- team-members/src/Employees.tsx
+++ team-members/src/Employees.tsx
@@ -1,112 +1,49 @@
-import {useState} from "react";
 // @ts-ignore
 import femaleProfile from './images/femaleProfile.png';
+// @ts-ignore
+import maleProfile from './images/maleProfile.png';
 
-const initialState = [
-    {
-        id: 1,
-        fullName: "Bob Jones",
-        designation: "Javascript Developer",
-        gender: "male",
-        teamName: "teamA"
-    },
-    {
-        id: 2,
-        fullName: "Jill Bailey",
-        designation: "Node Developer",
-        gender: "female",
-        teamName: "teamA"
-    },
-    {
-        id: 3,
-        fullName: "Gail Shepherd",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamA"
-    },
-    {
-        id: 4,
-        fullName: "Alex",
-        designation: "Node Developer",
-        gender: "female",
-        teamName: "teamB"
-    },
-    {
-        id: 5,
-        fullName: "Bob",
-        designation: "Javascript Developer",
-        gender: "male",
-        teamName: "teamB"
-    },
-    {
-        id: 6,
-        fullName: "Jill",
-        designation: "Node Developer",
-        gender: "female",
-        teamName: "teamB"
-    },
-    {
-        id: 7,
-        fullName: "Shepherd",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamC"
-    },
-    {
-        id: 8,
-        fullName: "Kai",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamC"
-    },
-    {
-        id: 9,
-        fullName: "Oriana",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamC"
-    },
-    {
-        id: 10,
-        fullName: "Dr.Jax",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamD"
-    },
-    {
-        id: 11,
-        fullName: "Dr.Swain",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamD"
-    },
-    {
-        id: 12,
-        fullName: "Lee shin",
-        designation: "Java Developer",
-        gender: "male",
-        teamName: "teamD"
-    }
-]
-const Employees = () => {
-    const [employees, setEmployees] = useState(initialState);
+
+// @ts-ignore
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
 
     return (
         <main className="container">
             <div className="row justify-content-center mt-3 mb-3">
                 <div className="col-8">
-                    {
-                        employees.map((emp) => (
-                            <div key={emp.id} className="card">
-                                <img src={femaleProfile} className="card-img-top"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Full name: {emp.fullName}</h5>
-                                    <p className="card-text">Designation: {emp.designation}</p>
-                                </div>
+                    <select className="form-select form-select-lg" value={selectedTeam}
+                            onChange={handleTeamSelectionChange}>
+                        <option value="teamA">Team A</option>
+                        <option value="teamB">Team B</option>
+                        <option value="teamC">Team C</option>
+                        <option value="teamD">Team D</option>
+                    </select>
+                </div>
+            </div>
+            <div className="row justify-content-center mt-3 mb-3">
+                <div className="col-8">
+                    <div className="card-collection">
+                        {
+                            employees?.map((emp: any) => (
+                                <div key={emp.id}
+                                     className={emp.teamName === selectedTeam ? "card m-2 standout" : 'card m-2'}
+                                     style={{cursor: "pointer"}}
+                                     onClick={handleEmployeeCardClick}>
 
-                            </div>
-                        ))
-                    }
+                                    {(emp.gender === 'male') ? <img src={maleProfile} className="card-img-top"/>
+                                        : <img src={femaleProfile} className="card-img-top"/>
+                                    }
+
+
+                                    <div className="card-body">
+                                        <h5 className="card-title"><b>Full name: {emp.fullName}</b></h5>
+                                        <p className="card-text"><b>Designation</b>: {emp.designation}</p>
+                                    </div>
+
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </main>
